af8846490e207a41fa64d2e0b747572304ab7308
NolanBourdeilh <nolan.bourdeilh@ynov.com>
2026-05-10 14:43:43 +0200

    create file for forgot and reset password page and add path to them

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound.tsx'
+import ForgotPassword from './pages/ForgotPassword.tsx'
+import ResetPassword from './pages/ResetPassword.tsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="forgot-password" element={<ForgotPassword />} />
+      <Route path="reset-password" element={<ResetPassword />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
