0ef3602e9c18f783d76986668c24abaf0a884140
NolanBourdeilh <nolan.bourdeilh@ynov.com>
2026-05-11 09:52:14 +0200

    add CORS middleware to allow frontend requests

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
 	"github.com/enzo959/forumium/config"
-
 	"github.com/enzo959/forumium/routes"
 )
 
@@ -20,6 +20,13 @@ func main() {
 	config.ConnectDatabase()
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+    AllowOrigins:     []string{"http://localhost:5173"},
+    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+    AllowCredentials: true,
+	}))
 
 	routes.SetupRoutes(router)
 
