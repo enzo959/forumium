3049a0111bd73dbed96ea61b3c5b72fabb920015
NolanBourdeilh <nolan.bourdeilh@ynov.com>
2026-05-22 09:04:19 +0200

    feat: add path for PostDetail Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,13 @@ import ForgotPassword from './pages/ForgotPassword.tsx'
 import ResetPassword from './pages/ResetPassword.tsx'
 import Login from './pages/Login.tsx'
 import Register from './pages/Register.tsx'
+import PostDetail from './pages/PostDetail'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/posts/:id" element={<PostDetail />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
       <Route path="reset-password" element={<ResetPassword />} />
       <Route path="login" element={<Login />} />
