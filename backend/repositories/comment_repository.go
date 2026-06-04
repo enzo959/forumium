c8d71a7eedbe8ddf3daf1578cfe8b01c5655e518
NolanBourdeilh <nolan.bourdeilh@ynov.com>
2026-06-04 21:44:52 +0200

    feat: add GetCommentByID and DeleteComment to comment repository

--- backend/repositories/comment_repository.go
+++ backend/repositories/comment_repository.go
@@ -7,20 +7,26 @@ import (
 
 func GetCommentsByPostID(postID uint) ([]models.Comment, error) {
 	var comments []models.Comment
-
-	err := config.DB.
-		Preload("User").
-		Where("post_id = ?", postID).
-		Order("created_at ASC").
-		Find(&comments).Error
-
+	err := config.DB.Preload("User").Where("post_id = ?", postID).Order("created_at ASC").Find(&comments).Error
 	if err != nil {
 		return nil, err
 	}
-
 	return comments, nil
 }
 
 func CreateComment(comment *models.Comment) error {
 	return config.DB.Create(comment).Error
+}
+
+func GetCommentByID(id uint) (*models.Comment, error) {
+	var comment models.Comment
+	err := config.DB.First(&comment, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &comment, nil
+}
+
+func DeleteComment(comment *models.Comment) error {
+	return config.DB.Delete(comment).Error
 }
