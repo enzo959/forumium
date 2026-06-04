447fa39b109bf9d23c82dbf3935ce15d3a2f18c7
NolanBourdeilh <nolan.bourdeilh@ynov.com>
2026-06-04 20:56:59 +0200

    feat: add unique constraint on user_id and post_id in reactions table

--- backend/models/models.go
+++ backend/models/models.go
@@ -13,14 +13,14 @@ const (
 )
 
 type User struct {
-	gorm.Model   // permet de faire ID, CreateAt, UpdateAt et DeleteAt
-	UserName string `gorm:"column:username;not null;unique" json:"username"`
-	Password string `gorm:"column:password;not null"        json:"password"`
-	Email    string `gorm:"column:email;not null;unique"    json:"email"`
-	Avatar   string `gorm:"column:avatar"                   json:"avatar"`
-	Posts        []Post        `gorm:"foreignKey:UserID"`
-	Comments     []Comment     `gorm:"foreignKey:UserID"`
-	Reactions    []Reaction    `gorm:"foreignKey:UserID"`
+	gorm.Model
+	UserName      string         `gorm:"column:username;not null;unique" json:"username"`
+	Password      string         `gorm:"column:password;not null"        json:"password"`
+	Email         string         `gorm:"column:email;not null;unique"    json:"email"`
+	Avatar        string         `gorm:"column:avatar"                   json:"avatar"`
+	Posts         []Post         `gorm:"foreignKey:UserID"`
+	Comments      []Comment      `gorm:"foreignKey:UserID"`
+	Reactions     []Reaction     `gorm:"foreignKey:UserID"`
 	RefreshTokens []RefreshToken `gorm:"foreignKey:UserID"`
 }
 
@@ -30,7 +30,7 @@ type PasswordReset struct {
 	Token     string    `gorm:"column:token;not null"   json:"token"`
 	ExpiresAt time.Time `gorm:"column:expires_at"       json:"expiresAt"`
 	Used      bool      `gorm:"column:used;default:false" json:"used"`
-	User User `gorm:"foreignKey:UserID"`
+	User      User      `gorm:"foreignKey:UserID"`
 }
 
 type RefreshToken struct {
@@ -39,15 +39,15 @@ type RefreshToken struct {
 	Token     string    `gorm:"column:token;not null"   json:"token"`
 	ExpiresAt time.Time `gorm:"column:expires_at"       json:"expiresAt"`
 	Revoked   bool      `gorm:"column:revoked;default:false" json:"revoked"`
-	User User `gorm:"foreignKey:UserID"`
+	User      User      `gorm:"foreignKey:UserID"`
 }
 
 type Post struct {
 	gorm.Model
-	UserID  uint   `gorm:"column:user_id;not null" json:"userId"`
-	Title   string `gorm:"column:title;not null"   json:"title"`
-	Content string `gorm:"column:content;not null" json:"content"`
-	Image   string `gorm:"column:image"            json:"image"`
+	UserID     uint       `gorm:"column:user_id;not null" json:"userId"`
+	Title      string     `gorm:"column:title;not null"   json:"title"`
+	Content    string     `gorm:"column:content;not null" json:"content"`
+	Image      string     `gorm:"column:image"            json:"image"`
 	User       User       `gorm:"foreignKey:UserID"`
 	Comments   []Comment  `gorm:"foreignKey:PostID"`
 	Reactions  []Reaction `gorm:"foreignKey:PostID"`
@@ -59,21 +59,21 @@ type Comment struct {
 	UserID  uint   `gorm:"column:user_id;not null" json:"userId"`
 	PostID  uint   `gorm:"column:post_id;not null" json:"postId"`
 	Content string `gorm:"column:content;not null" json:"content"`
-	User User `gorm:"foreignKey:UserID"`
-	Post Post `gorm:"foreignKey:PostID"`
+	User    User   `gorm:"foreignKey:UserID"`
+	Post    Post   `gorm:"foreignKey:PostID"`
 }
 
 type Reaction struct {
 	gorm.Model
-	UserID uint         `gorm:"column:user_id;not null" json:"userId"`
-	PostID uint         `gorm:"column:post_id;not null" json:"postId"`
-	Type   ReactionType `gorm:"column:type;not null"    json:"type"`
-	User User `gorm:"foreignKey:UserID"`
-	Post Post `gorm:"foreignKey:PostID"`
+	UserID uint         `gorm:"column:user_id;not null;uniqueIndex:idx_user_post_reaction" json:"userId"`
+	PostID uint         `gorm:"column:post_id;not null;uniqueIndex:idx_user_post_reaction" json:"postId"`
+	Type   ReactionType `gorm:"column:type;not null"                                       json:"type"`
+	User   User         `gorm:"foreignKey:UserID"`
+	Post   Post         `gorm:"foreignKey:PostID"`
 }
 
 type Category struct {
 	gorm.Model
-	Name string `gorm:"column:name;not null;unique" json:"name"`
+	Name  string `gorm:"column:name;not null;unique" json:"name"`
 	Posts []Post `gorm:"many2many:post_categories"`
 }
